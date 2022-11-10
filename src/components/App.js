41691939b4aff6929ca9755251bd3bd97cbe4d78
GoRuGoo <dachanyr8989@gmail.com>
2022-11-10 18:15:39 +0900

    add: DNA読み込み完了

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import Footer from './Footer'
-
-
+import { Dna } from 'react-loader-spinner'
 class App extends React.Component {
     render() {
         return (
             <div>
                 <Footer />
+                <Dna
+                    visible={true}
+                    height="80"
+                    width="80"
+                    ariaLabel="dna-loading"
+                    wrapperStyle={{}}
+                    wrapperClass="dna-wrapper"
+                />
             </div>
         )
     }
