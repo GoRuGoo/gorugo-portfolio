d3104da2cf8c3ff02bee65454472d27b99830a30
GoRuGoo <dachanyr8989@gmail.com>
2022-11-11 09:57:52 +0900

    add: 読み込み画面追加

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,39 @@ import React from 'react'
 import Footer from './Footer'
 import { Dna } from 'react-loader-spinner'
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: false
+        }
+    }
+    componentDidMount() {
+        setTimeout(() => this.setState({ loading: true }), 5000)
+    }
     render() {
-        return (
-            <div>
-                <Footer />
-                <Dna
-                    visible={true}
-                    height="80"
-                    width="80"
-                    ariaLabel="dna-loading"
-                    wrapperStyle={{}}
-                    wrapperClass="dna-wrapper"
-                />
-            </div>
-        )
+        if (!this.state.loading) {
+            return (
+                <div>
+                    <Dna
+                        visible={true}
+                        height="500"
+                        width="500"
+                        ariaLabel="dna-loading"
+                        wrapperStyle={{}}
+                        wrapperClass="dna-wrapper"
+                    />
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <Footer />
+                </div>
+            )
+        }
     }
 }
+
 
 
 
