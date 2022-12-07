7c621d0f317b40b960468cbc8370902df6607f0d
GoRuGoo <dachanyr8989@gmail.com>
2022-12-08 04:20:46 +0900

    add: JSでpaddingを操作できるようにした

---
+++ main.js
@@ -0,0 +1,6 @@
+var windowWidth = window.innerWidth;
+var windowHeight = window.innerHeight;
+console.log(windowWidth);
+console.log(windowHeight);
+
+document.getElementById('welcome-div-class').style.padding = "800px";
