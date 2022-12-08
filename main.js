1644683a7e0931e294887f0d3b48ceac765e3ff7
GoRuGoo <dachanyr8989@gmail.com>
2022-12-08 09:50:41 +0900

    add: ある程度整っている＆hrに試行錯誤

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 var windowWidth = window.innerWidth;
 document.getElementById('welcome-div-class').style.paddingTop = String(window.innerHeight/2)+"px";
 document.getElementById('welcome-div-class').style.paddingBottom = String(window.innerHeight/2)+ "px";
+
+
+
+window.onload = ()=>{
+    const loader = document.getElementById('loader');
+    loader.classList.add('loaded');
+}
