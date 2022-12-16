b177f47ab7bd9e37453249ebe0e2efac934f835f
GoRuGoo <dachanyr8989@gmail.com>
2022-12-17 00:10:46 +0900

    fix: 文字の左右の間隔を自動調整できるよにした

--- static/animate.js
+++ static/animate.js
@@ -1,41 +1,45 @@
-// ScrollReveal().reveal('.first-content', {
-//   duration: 300,
-//   delay: 300,
-//   opacity: 0,
-//   reset: true
-// });
+ScrollReveal().reveal('.first-content', {
+  duration: 300,
+  delay: 300,
+  opacity: 0,
+  reset: true
+});
 
-// ScrollReveal().reveal('#biography-div', {
-//   duration: 1600,
-//   origin: 'right',
-//   distance: '50%',
-//   reset: true
-// });
+ScrollReveal().reveal('#biography-div', {
+  duration: 1600,
+  origin: 'right',
+  distance: '50%',
+  reset: true,
+  opacity:0
+});
 
-// ScrollReveal().reveal('#introduce-my-skills-div', {
-//   duration: 1600,
-//   origin: 'right',
-//   distance: '50%',
-//   reset: true
-// });
+ScrollReveal().reveal('#introduce-my-skills-div', {
+  duration: 1600,
+  origin: 'right',
+  distance: '50%',
+  reset: true,
+});
 
-// ScrollReveal().reveal('#my-awards-and-qua-div', {
-//   duration: 1600,
-//   origin: 'right',
-//   distance: '50%',
-//   reset: true
-// });
+ScrollReveal().reveal('#my-awards-and-qua-div', {
+  duration: 1600,
+  origin: 'right',
+  distance: '50%',
+  reset: true,
+  opacity:0
+});
 
-// ScrollReveal().reveal('#my-favorite-div', {
-//   duration: 1600,
-//   origin: 'right',
-//   distance: '50%',
-//   reset: true
-// });
+ScrollReveal().reveal('#my-favorite-div', {
+  duration: 1600,
+  origin: 'right',
+  distance: '50%',
+  reset: true,
+  opacity:0
+});
 
-// ScrollReveal().reveal('#contact-div', {
-//   duration: 1600,
-//   origin: 'right',
-//   distance: '50%',
-//   reset: true
-// });
+ScrollReveal().reveal('#contact-div', {
+  duration: 1600,
+  origin: 'right',
+  distance: '50%',
+  reset: true,
+  opacity:0
+});
